91609382490a4be9e33a6ba40dbd73166372e402
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2011-03-06 23:31:09 +0100

    Add "description" method to FlickrMemories.Photo

--- flickr-memories.scala
+++ flickr-memories.scala
@@ -5,8 +5,10 @@ import java.util.{Calendar,Date}
 import java.text.SimpleDateFormat
 
 package FlickrMemories {
-  class Photo(id: String, secret: String, userId: String,
-              farmId: String, serverId: String, title: String) {
+  class Photo(var id: String, var secret: String, var userId: String,
+              var farmId: String, var serverId: String, var title: String) {
+    var _description : String = null
+
     def imageUrl : String = {
       return "http://farm" + farmId + ".static.flickr.com/" +
                 serverId + "/" + id + "_" + secret + ".jpg"
@@ -14,6 +16,27 @@ package FlickrMemories {
 
     def pageUrl : String = {
       return "http://www.flickr.com/photos/" + userId + "/" + id
+    }
+
+    def extractDescriptionFromFlickResponse(xml: String) : String = {
+      val dom = XML.loadString(xml)
+      return ((dom \\ "photo")(0) \ "description").text
+    }
+
+    def fetchPhotoDescription : String = {
+      val xml = Http("http://api.flickr.com/services/rest").
+                  param("method",         "flickr.photos.getinfo").
+                  param("api_key",        App.API_KEY).
+                  param("photo_id",       id).asString
+      return extractDescriptionFromFlickResponse(xml)
+    }
+
+    def description : String = {
+      if (_description == null) {
+        _description = fetchPhotoDescription
+      }
+
+      return _description
     }
   }
 
