29579e028af5aaef42a2676bad82a2caaacef2b4
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2011-03-22 22:07:11 +0100

    Make the code more testable, fix a bug (?!)
    * Separate the code that parses the XML from Flickr to a new method
      searchResultXmlToPhotoSeq
    * Use "PHOTO", not "photo", in the XPath-like queries. For some reason it
      stopped working and now I have to use "PHOTO" (???!!!)

--- flickr-memories.scala
+++ flickr-memories.scala
@@ -54,7 +54,7 @@ package FlickrMemories {
 
     def extractDescriptionFromFlickResponse(xml: String) : String = {
       val dom = XML.loadString(xml)
-      return ((dom \\ "photo")(0) \ "description").text
+      return ((dom \\ "PHOTO")(0) \ "description").text
     }
 
     def fetchPhotoDescription : String = {
@@ -86,17 +86,21 @@ package FlickrMemories {
                 param("max_taken_date", dateSpecUntil).asString
     }
 
-    def searchByUserAndDateRange(userId: String, dateSpecSince: String,
-                                 dateSpecUntil: String) : Seq[Photo] = {
-      val dom = XML.loadString(rawSearchByUserAndDateRange(userId,
-                                                           dateSpecSince,
-                                                           dateSpecUntil))
-      return (dom \\ "photo").map {p => new Photo((p \ "@id").text,
+    def searchResultXmlToPhotoSeq(xml: String) : Seq[Photo] = {
+      val dom = XML.loadString(xml)
+      return (dom \\ "PHOTO").map {p => new Photo((p \ "@id").text,
                                                   (p \ "@secret").text,
                                                   (p \ "@owner").text,
                                                   (p \ "@farm").text,
                                                   (p \ "@server").text,
                                                   (p \ "@title").text)}
+    }
+
+    def searchByUserAndDateRange(userId: String, dateSpecSince: String,
+                                 dateSpecUntil: String) : Seq[Photo] = {
+      searchResultXmlToPhotoSeq(rawSearchByUserAndDateRange(userId,
+                                                            dateSpecSince,
+                                                            dateSpecUntil))
     }
   }
 
